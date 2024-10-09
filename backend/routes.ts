3e0dd742f1c836e131072484d3639a6335b30200
Anita Yumi Kawasaki <aanitakawasaki@gmail.com>
2024-10-09 13:57:35 -0300

    configurações iniciais

--- backend/routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-// router.get("/users/", authenticate,(req, res) => {
-//   return listUsersController.handle(req, res)
-// });
-
-export { router };
