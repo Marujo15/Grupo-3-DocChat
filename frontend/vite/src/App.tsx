71836119c7ea1bdfbefb5540121bba63a0a66f2d
Anita Yumi Kawasaki <aanitakawasaki@gmail.com>
2024-10-11 13:58:46 -0300

    testando integração frontend com backend

--- frontend/vite/src/App.tsx
+++ frontend/vite/src/App.tsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [testData, setTestData] = useState<any>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`http://localhost:3000/api/test`);
+        const jsonResponse = await response.json();
+        setTestData(jsonResponse);
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -17,6 +32,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + ReactAAA</h1>
+      <p>{testData ? JSON.stringify(testData) : "Carregando..."}</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
