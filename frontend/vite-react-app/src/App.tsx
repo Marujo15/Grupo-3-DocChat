89de071e3d81ae60dd58ba9480627e7877c9b551
Anita Yumi Kawasaki <aanitakawasaki@gmail.com>
2024-10-23 20:50:19 -0300

    criando About Us

--- frontend/vite-react-app/src/App.tsx
+++ frontend/vite-react-app/src/App.tsx
@@ -8,44 +8,46 @@ import ChatArea from "./components/ChatArea/ChatArea";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import UserPage from "./components/UserPage/UserPage";
+import AboutUs from "./components/AboutUs/AboutUs";
 
 const MainPage: React.FC = () => {
-    const { user } = useAuth();
-    return (
-        <>
-            <Header variant="default" />
-            <Carousel userId={user ? user.id : ""} />
-            <ChatArea userId={user ? user.id : ""} />
-        </>
-    );
+  const { user } = useAuth();
+  return (
+    <>
+      <Header variant="default" />
+      <Carousel userId={user ? user.id : ""} />
+      <ChatArea userId={user ? user.id : ""} />
+    </>
+  );
 };
 
 const UserProfilePage: React.FC = () => {
-    const { user } = useAuth();
-    return (
-        <>
-            <UserPage userId={user ? user.id : ""} />
-        </>
-    );
+  const { user } = useAuth();
+  return (
+    <>
+      <UserPage userId={user ? user.id : ""} />
+    </>
+  );
 };
 
 const App: React.FC = () => {
-    return (
-        <AuthProvider>
-            <ChatProvider>
-                <Router>
-                    <Routes>
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/user" element={<UserProfilePage />} />
-                        <Route path="/" element={<MainPage />} />
-                        <Route path="/chat/:chatId" element={<MainPage />} />
-                        <Route path="/chat/" element={<MainPage />} />
-                    </Routes>
-                </Router>
-            </ChatProvider>
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <ChatProvider>
+        <Router>
+          <Routes>
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/user" element={<UserProfilePage />} />
+            <Route path="/" element={<MainPage />} />
+            <Route path="/chat/:chatId" element={<MainPage />} />
+            <Route path="/chat/" element={<MainPage />} />
+          </Routes>
+        </Router>
+      </ChatProvider>
+    </AuthProvider>
+  );
 };
 
 export default App;
---
+++ frontend/vite-react-app/src/components/AboutUs/AboutUs.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Header from "../Header/Header";
+import "./AboutUs.css";
+
+const AboutUs: React.FC = () => {
+  return (
+    <>
+      <Header variant="aboutus" />
+      <div className="aboutus-container">
+        <div className="aboutus-div">
+          <p>
+            Nossa plataforma de chat interativo permite a{" "}
+            <strong>exploração de documentações técnicas</strong> de maneira
+            eficiente, integrando scraping automatizado, processamento semântico
+            e inteligência artificial. Ela funciona da seguinte maneira:
+          </p>
+          <ul>
+            <li>
+              Os usuários podem <strong>submeter URLs de documentações</strong>,
+              que são processadas pelo sistema para a extração de dados,{" "}
+              <strong>transformados em vetores</strong> e{" "}
+              <strong>armazendos em um banco de dados vetorial</strong>.
+            </li>
+            <li>
+              Após esse processo, o usuário pode fazer uma{" "}
+              <strong>pergunta ao chat</strong>, que realizará uma{" "}
+              <strong>busca por similaridade</strong> no banco de dados vetorial
+              e conseguirá oferecer{" "}
+              <strong>respostas contextualizadas e precisas</strong>.
+            </li>
+          </ul>
+          <p>
+            Essa combinação de <strong>scraping</strong>,{" "}
+            <strong>processamento semântico</strong> e <strong>IA</strong> torna
+            nossa plataforma uma solução prática para profissionais que precisam
+            acessar e explorar documentações técnicas de forma rápida e
+            personalizada.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AboutUs;
